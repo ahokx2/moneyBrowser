d9456028b3cf8871ed0720f090dc74ef33a91f29
ahokx2 <ahokx2@gmail.com>
2025-04-07 22:09:38 +0700

    Add files via upload

--- models.py
+++ models.py
@@ -3,11 +3,33 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class tblthuchi(db.Model):
+    __tablename__ = 'tblthuchi'
     id = db.Column(db.Integer, primary_key=True)
     ngay = db.Column(db.String(100), nullable=True)
     loai_giao_dich = db.Column(db.String(100), nullable=True)
-    nguon_tien = db.Column(db.String(100), nullable=True)
-    noi_den = db.Column(db.String(100), nullable=True)
+    nguon_tien = db.Column(db.String(100), db.ForeignKey('tblnguonnoiden.ma_nguon'), nullable=True)
+    noi_den = db.Column(db.String(100), db.ForeignKey('tblnguonnoiden.ma_nguon'), nullable=True)
     so_tien = db.Column(db.Float, nullable=True)
     ghi_chu = db.Column(db.String(100), nullable=True)
-    danh_muc = db.Column(db.String(100), nullable=True)
+    danh_muc = db.Column(db.String(100), db.ForeignKey('tbldanhmuc.ma_danh_muc'),
+                          nullable=True)
+    
+    danh_muc_obj = db.relationship('tbldanhmuc', backref='danh_muc', lazy=True)
+
+    # liên kết với bảng tblnguontiennoiden, chỉ rõ foreign_keys
+    nguon_tien_obj = db.relationship('tblnguonnoiden', backref='nguon_tien', lazy=True, foreign_keys=[nguon_tien])
+    noi_den_obj = db.relationship('tblnguonnoiden', backref='noi_den', lazy=True, foreign_keys=[noi_den])
+
+
+
+class tbldanhmuc(db.Model):
+    __tablename__ = 'tbldanhmuc'
+    ma_danh_muc = db.Column(db.String(100), nullable=True, primary_key=True)
+    ten_danh_muc = db.Column(db.String(100), nullable=True)  # 'thu' hoặc 'chi'
+
+class tblnguonnoiden(db.Model):
+    __tablename__ = 'tblnguonnoiden'
+    ma_nguon = db.Column(db.String(100), nullable=True, primary_key=True)
+    ten_nguon = db.Column(db.String(100), nullable=True)
+    loai_tai_san = db.Column(db.String(100), nullable=True)
+    ghi_chu = db.Column(db.String(100), nullable=True)
