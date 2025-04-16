04906070c523662c03cfbff01572013afe8d402e
ahokx2 <ahokx2@gmail.com>
2025-04-16 22:53:56 +0700

    Add files via upload

--- routes.py
+++ routes.py
@@ -2,8 +2,9 @@ import os
 from flask import Flask, render_template, request, redirect, flash, url_for, jsonify
 from datetime import date
 from db_utils import get_giao_dich_all, get_tong_hop_giao_dich_all, get_tong_hop_giao_dich_by_month, get_tong_hop_giao_dich_thang_hien_tai, add_giao_dich_moi, get_5_giao_dich_gan_nhat, get_giao_dich_by_date, get_giao_dich_thang_hien_tai, get_giao_dich_by_date, get_bieu_do_phan_tram_nguon_tien, get_bieu_do_phan_tram_nguon_tien_by_month
-from models import db, tblnguonnoiden, tbldanhmuc, tblthuchi, User, vbieudophantramnguontien
+from models import db, tblnguonnoiden, tbldanhmuc, tblthuchi, User, vbieudophantramnguontien, vtonghopgiaodich
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user, UserMixin
+import unicodedata
 
 app = Flask(__name__)
 
@@ -11,7 +12,15 @@ app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 db_path = os.path.join(basedir, 'moneyBrowser.db3')  # nếu db nằm ngoài thư mục chứa routes.py
 
-app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+# app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+
+#cái này là external, dùng để dev tại máy cá nhân
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ahokx2:DE2xekSNBsFUCcechR6u84bnnWvPDYkH@dpg-cvui9ommcj7s73ceqjjg-a.oregon-postgres.render.com/moneybrowser_44q3'
+
+# cái này là internal, dùng trên render
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ahokx2:DE2xekSNBsFUCcechR6u84bnnWvPDYkH@dpg-cvui9ommcj7s73ceqjjg-a/moneybrowser_44q3'
+
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'some_very_secret_key'
 
@@ -20,6 +29,13 @@ login_manager.login_view = 'login'  # tên của route login
 login_manager.init_app(app)
 
 db.init_app(app)
+
+def remove_accents(text):
+    text = text.replace('đ', 'd').replace('Đ', 'D')  # xử lý riêng
+    nfkd_form = unicodedata.normalize('NFKD', text)
+    return "".join([c for c in nfkd_form if not unicodedata.combining(c)]).lower().replace(" ", "")
+
+app.jinja_env.filters['slugify'] = remove_accents
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -64,6 +80,53 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for("login"))
+
+@app.route('/api/data')
+def api_data():
+    loai = request.args.get("loai")
+    today = date.today().strftime('%Y-%m-%d')
+
+    if loai == 'tblthuchi':
+        data = tblthuchi.query.order_by(tblthuchi.ngay.desc()).all()
+        result = [{
+            "ngay": g.ngay,
+            "loai_giao_dich": g.loai_giao_dich,
+            "so_tien": g.so_tien,
+            "ghi_chu": g.ghi_chu
+        } for g in data]
+
+        return jsonify({
+            "success": True,
+            "tieu_de": "Danh sách giao dịch chi tiết",
+            "data": result
+        })
+
+    elif loai == 'tonghop':
+        data = vtonghopgiaodich.query.order_by(vtonghopgiaodich.thang.desc()).all()
+        result = [{
+            "thang": i.thang,
+            "nguon_tien": i.nguon_tien,
+            "ten_nguon": i.ten_nguon,
+            "dau_ky": i.dau_ky,
+            "thu": i.thu,
+            "chi": i.chi,
+            "chuyen_doi": i.chuyen_doi,
+            "cuoi_ky": i.cuoi_ky
+        } for i in data]
+
+        return jsonify({
+            "success": True,
+            "tieu_de": "Báo cáo tổng hợp theo tháng",
+            "data": result
+        })
+
+    else:
+        return jsonify({
+            "success": False,
+            "tieu_de": "Lỗi",
+            "message": "Không xác định được loại yêu cầu",
+            "data": []
+    })
 
 @app.route('/api/phantram-nguontien')
 def get_bieu_do_nguon_tien():
@@ -113,40 +176,34 @@ def loc_theo_ngay():
     
 @app.route("/", methods = ["GET"])
 @login_required
-def index():
-    # giao_dich_hom_nay = get_giao_dich_hom_nay()
-    # giao_dich_gan_nhat = get_5_giao_dich_gan_nhat()
-    # tong_hop_giao_dich = get_tong_hop_giao_dich_all()
-    # tong_hop_giao_dich_by_month = get_tong_hop_giao_dich_by_month("03/2025")
-    # tong_hop_giao_dich_thang_hien_tai = get_tong_hop_giao_dich_thang_hien_tai()
-    danh_sach_nguon = tblnguonnoiden.query.all()
-    danh_sach_danh_muc = tbldanhmuc.query.all()
+def index():    
+    loai = request.args.get('loai')
+    data = []
+    data1 = []
+    tieu_de = "Chào mừng!"
+    tieu_de_data = ""
+    tieu_de_data1 = ""
+    kieu = None  # để điều khiển kiểu hiển thị trong template
 
-    ngay_loc = request.args.get("ngay_loc")  # xem giao dịch theo ngày
-    xem_thang = request.args.get("xem_thang") # xem giao dịch tháng hiện tại
+    if loai == 'tblthuchi':
+        data = tblthuchi.query.order_by(tblthuchi.ngay.desc()).all()
+        tieu_de = "Danh sách giao dịch chi tiết"
+        kieu = 'chi_tiet'
 
-    if ngay_loc:
-        # Convert sang dd/mm/yyyy nếu mày lưu vậy trong DB
-        parts = ngay_loc.split("-")
-        formatted = f"{parts[2]}/{parts[1]}/{parts[0]}"
-        giao_dich = get_giao_dich_by_date(formatted) # lấy giao dịch theo ngày
-        title = f"Giao dịch ngày {formatted}"
-    elif xem_thang:
-        giao_dich = get_giao_dich_thang_hien_tai() # lấy giao dịch tháng hiện tại
-        title = "Giao dịch tháng hiện tại : " + thang_hien_tai
-    else:
-        giao_dich = get_5_giao_dich_gan_nhat() # lấy 5 giao dịch gần nhất
-        title = "5 giao dịch gần nhất"
-        bieu_do_thang_hien_tai = get_bieu_do_phan_tram_nguon_tien_by_month(thang_hien_tai) # lấy biểu đồ theo tháng hiện tại
+    elif loai == 'tonghop':
+        data = vtonghopgiaodich.query.order_by(vtonghopgiaodich.thang.desc()).all()
+        tieu_de = "Báo cáo tổng hợp theo tháng"
+        kieu = 'tong_hop'
 
-    return render_template('index.html',
-                           today = hom_nay,
-                           thang_hien_tai = thang_hien_tai,
-                           title = title,
-                           giao_dich = giao_dich,
-                           bieu_do_thang_hien_tai = bieu_do_thang_hien_tai,
-                           danh_sach_nguon = danh_sach_nguon,
-                           danh_sach_danh_muc = danh_sach_danh_muc)
+    else :
+        data = get_5_giao_dich_gan_nhat()
+        data1 = get_bieu_do_phan_tram_nguon_tien_by_month(thang_hien_tai)
+        tieu_de = "Tổng quan tài chính"
+        tieu_de_data = "Danh sách giao dịch gần nhất"
+        tieu_de_data1 = "Phần trăm nguồn tiền"
+        kieu = 'tong_quan_tai_chinh'
+
+    return render_template('index.html', tieu_de=tieu_de, data=data, data1=data1, kieu=kieu, tieu_de_data=tieu_de_data, tieu_de_data1=tieu_de_data1)
 
 if __name__ == "__main__":
     app.run(debug=True)
